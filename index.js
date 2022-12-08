33d37d4efabc1bf2ffd2f35deb4bc7c5aef4182e
Jens <69081683+jenslys@users.noreply.github.com>
2022-12-08 15:41:25 +0100

    auth_token generation
    added automatic auth_token generation after 30 days or if the ENV is empty

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ require ('dotenv').config();
 const express = require('express');
 const fetch = require('node-fetch');
 const rateLimit = require('express-rate-limit')
-
+const schedule = require('node-schedule');
 const app = express();
-const baseUrl = 'https://api.twitch.tv/helix';
-const clientId = process.env.TWITCH_CLIENT_ID;
-const authToken = process.env.TWITCH_AUTH_TOKEN;
-const port = process.env.PORT || 3000;
 
+const baseUrl = 'https://api.twitch.tv/helix';
+const client_id = process.env.TWITCH_CLIENT_ID;
+const client_secret = process.env.TWITCH_CLIENT_SECRET;
+let authToken = process.env.TWITCH_AUTH_TOKEN || generateAuthToken(); // If there is no auth token in the environment variables, generate one
+const port = process.env.PORT || 3000;
 const limiter = rateLimit({
   // limits it to 2 requests per minute
 	windowMs: 60000,
@@ -18,37 +19,56 @@ const limiter = rateLimit({
 })
 app.use(limiter) // applies the rate limit to all requests
 
+async function generateAuthToken() {
+    const body = new URLSearchParams({
+      client_id,
+      client_secret,
+      grant_type: "client_credentials",
+    });
+
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body,
+    };
+
+    const response = await fetch("https://id.twitch.tv/oauth2/token", options);
+    const data = await response.json();
+    authToken = data.access_token;
+}
+const job = schedule.scheduleJob('00 00 00 28,29,30 * *', function(){
+  generateAuthToken();
+});
 
 app.get('/title/:username', async (req, res) => {
   const username = req.params.username;
   const response = await fetch(`${baseUrl}/streams?user_login=${username}`, {
     headers: {
-      'Client-ID': clientId,
+      'Client-ID': client_id,
       'Authorization': 'Bearer ' + authToken
     }
   });
 
   const json = await response.json();
-  const streamTitle = json.data?.[0]?.title;
+  const stream_title = json.data?.[0]?.title;
 
-  res.send({ streamTitle });
+  res.send({ stream_title });
 });
 
 app.get('/game/:username', async (req, res) => {
   const username = req.params.username;
   const response = await fetch(`${baseUrl}/streams?user_login=${username}`, {
     headers: {
-      'Client-ID': clientId,
+      'Client-ID': client_id,
       'Authorization': 'Bearer ' + authToken
     }
   });
 
   const json = await response.json();
-  const streamGame = json.data?.[0]?.game_name;
+  const stream_game = json.data?.[0]?.game_name;
 
-  res.send({ streamGame });
+  res.send({ stream_game });
 });
-
 
 
 app.listen(port, () => {
