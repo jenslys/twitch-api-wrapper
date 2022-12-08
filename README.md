88441d8495e5e37cd6e716f50772fb0a4ab38ebd
Jens <69081683+jenslys@users.noreply.github.com>
2022-12-08 13:00:33 +0100

    add endpoint to fetch game

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # Twitch API Wrapper
 
-Public API that wraps around the [Twitch Helix API](https://dev.twitch.tv/docs/api/) to fetch a streams title. This might be expanded in the future.
+Public API that wraps around the [Twitch Helix API](https://dev.twitch.tv/docs/api/) to fetch stream information. This might be expanded upon in the future.
 
-This was made as an companion for [AutoVOD](https://github.com/jenslys/autovod).
+This was made as an companion tool for [AutoVOD](https://github.com/jenslys/autovod).
 
 **Limitation**: The Twitch API dosent return anything if the streamer is not live.
 
-## Usage
+## Endpoints
 
-`http://my-url/title/<username-here>`
+`/title/<username-here>`
+
+`/game/<username-here>`
 
 ## Installation
 
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const limiter = rateLimit({
 	standardHeaders: true,
 	legacyHeaders: false,
 })
-app.use(limiter)
+app.use(limiter) // applies the rate limit to all requests
 
 
 app.get('/title/:username', async (req, res) => {
@@ -34,6 +34,23 @@ app.get('/title/:username', async (req, res) => {
   res.send({ streamTitle });
 });
 
+app.get('/game/:username', async (req, res) => {
+  const username = req.params.username;
+  const response = await fetch(`${baseUrl}/streams?user_login=${username}`, {
+    headers: {
+      'Client-ID': clientId,
+      'Authorization': 'Bearer ' + authToken
+    }
+  });
+
+  const json = await response.json();
+  const streamGame = json.data?.[0]?.game_name;
+
+  res.send({ streamGame });
+});
+
+
+
 app.listen(port, () => {
-  console.log("Listening on port" + port);
+  console.log("Listening on port: " + port);
 });
